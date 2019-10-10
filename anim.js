9c5cfdf4df108fcf66d1e1a9bacbb6ef379d6132
Nathan Carter <ncarter@bentley.edu>
2019-10-10 11:24:30 -0400

    Fixing to obey test file

--- anim.js
+++ anim.js
@@ -5,35 +5,60 @@ const w = 75, h = 75;
 function setColor ( c ) { color = c; }
 function setReverse ( r ) { reverse = r; update(); }
 
-function addBox ( text, x1, y1, x2, y2 ) {
+function addBox ( text, x1, y1, x2, y2, opt ) {
+    if ( !opt ) opt = { };
+    if ( !opt.w1 ) opt.w1 = 1;
+    if ( !opt.h1 ) opt.h1 = 1;
+    if ( !opt.w2 ) opt.w2 = 1;
+    if ( !opt.h2 ) opt.h2 = 1;
+    if ( !opt.color ) opt.color = color;
     var box = document.createElement( 'div' );
     box.innerHTML = '<p style="margin: auto;">' + text + '</p>';
     box.style.display = 'flex';
     box.style.border = '1px solid black';
-    box.style.width = w;
-    box.style.height = h;
-    box.style.background = color;
+    box.style.width = opt.w1 * w;
+    box.style.height = opt.h1 * h;
+    box.style.background = opt.color;
     main.appendChild( box );
     box.style.position = 'absolute';
     box.style.left = x1 * w;
     box.style.top = y1 * h;
-    box.limits = { x1 : x1 * w, y1 : y1 * h,
-                   x2 : x2 * w, y2 : y2 * h };
+    box.limits = {
+        x1 : x1 * w,
+        y1 : y1 * h,
+        x2 : x2 * w,
+        y2 : y2 * h,
+        w1 : opt.w1 * w,
+        h1 : opt.h1 * h,
+        w2 : opt.w2 * w,
+        h2 : opt.h2 * h,
+    };
     boxes.push( box );
 }
 
 function addBoxes () {
-    for ( var i = 0 ; i < arguments.length ; i += 5 )
-        addBox( arguments[i], arguments[i+1],
-                arguments[i+2], arguments[i+3], arguments[i+4] );
+    for ( var i = 0 ; i < arguments.length ; i += 5 ) {
+        if ( ( i+5 < arguments.length )
+          && ( arguments[i+5] instanceof Object ) ) {
+            addBox( arguments[i], arguments[i+1],
+                    arguments[i+2], arguments[i+3], arguments[i+4],
+                    arguments[i+5] );
+            i++;
+        } else {
+            addBox( arguments[i], arguments[i+1],
+                    arguments[i+2], arguments[i+3], arguments[i+4] );
+        }
+    }
 }
 
 function update () {
     boxes.map( box => {
         var t = slider.value / 100;
         if ( reverse ) t = 1 - t;
-        box.style.left = ( 1 - t ) * box.limits.x1 + t * box.limits.x2;
-        box.style.top  = ( 1 - t ) * box.limits.y1 + t * box.limits.y2;
+        box.style.left   = ( 1 - t ) * box.limits.x1 + t * box.limits.x2;
+        box.style.top    = ( 1 - t ) * box.limits.y1 + t * box.limits.y2;
+        box.style.width  = ( 1 - t ) * box.limits.w1 + t * box.limits.w2;
+        box.style.height = ( 1 - t ) * box.limits.h1 + t * box.limits.h2;
     } );
 }
 
